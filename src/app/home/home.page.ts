bbc8c71c9347b294d3cd2d7e5577ac779e928ff0
Arthur Masson <thur.masson@gmail.com>
2021-09-25 15:26:12 -0300

    Added classification and changed app colors

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,21 @@ export class HomePage {
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    var classification = '';
+
+    if (imc < 18.5)
+      classification = 'Magreza';
+    else if (imc <= 24.9)
+      classification = 'Normal';
+    else if (imc <= 29.9)
+      classification = 'Sobrepeso';
+    else if (imc <= 39.9)
+      classification = 'Obesidade';
+    else
+      classification = 'Obesidade grave';
+
+
+    this.showMessage(`IMC = ${imc.toFixed(2)} - ${classification}`);
   }
 
   async showMessage(msg: string) {
